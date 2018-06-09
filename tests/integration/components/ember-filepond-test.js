b4f6bc63381c7c5f0c6ec43a191600b40c3bc918
Alex DiLiberto <alex@alexdiliberto.com>
2018-06-09 13:59:08 -0400

    test(ember-filepond): ensure each test run completely waits until FilePond has rendered

--- tests/integration/components/ember-filepond-test.js
+++ tests/integration/components/ember-filepond-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, waitUntil } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | ember-filepond', function(hooks) {
@@ -10,6 +10,9 @@ module('Integration | Component | ember-filepond', function(hooks) {
     assert.expect(2);
 
     await render(hbs`{{ember-filepond}}`);
+
+    // NOTE: wait until FilePond has completely rendered
+    await waitUntil(() => this.element.querySelector('label').textContent.length);
 
     assert.dom('.filepond--root').exists({ count: 1 }, 'filepond upload component has been successfully rendered');
     assert.dom('label').includesText('Drag & Drop your files or Browse', 'default filepond instruction text');
